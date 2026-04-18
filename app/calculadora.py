6e23861c80c4f2357b7ea143a52e7ba849cc68b2
JuanM0412 <juangomezp1204@gmail.com>
2026-04-18 08:28:25 -0500

    Implement CI pipeline with Docker build and push

--- app/calculadora.py
+++ app/calculadora.py
@@ -9,18 +9,25 @@ AUTOR = "jmgomezp, mahoyosv, asjimenezm"
 
 
 def sumar(a, b):
+    """Suma dos números retornando su resultado."""
     return a + b
 
 
 def restar(a, b):
+    """Resta el segundo número al primero y devuelve el resultado."""
     return a - b
 
 
 def multiplicar(a, b):
+    """Multiplica dos números y devuelve el resultado."""
     return a * b
 
 
 def dividir(a, b):
+    """
+    Divide el primer número por el segundo.
+    Genera un error ZeroDivisionError si el segundo número es 0.
+    """
     if b == 0:
         raise ZeroDivisionError("No se puede dividir por cero")
     return a / b
