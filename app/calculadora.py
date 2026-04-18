368eb7626b0230518d7ac525412b25c963dbf0e4
JuanM0412 <juangomezp1204@gmail.com>
2026-04-17 19:39:01 -0500

    Initial commit

---
+++ app/calculadora.py
@@ -0,0 +1,19 @@
+AUTOR = "???"  # IMPORTANTE: Reemplaza con tu nombre completo (debe coincidir con el nombre que uses en cualquier otro identificador del proyecto. Sugerencia: Usuario de correo de EAFIT)
+
+
+def sumar(a, b):
+    return a + b
+
+
+def restar(a, b):
+    return a - b
+
+
+def multiplicar(a, b):
+    return a * b
+
+
+def dividir(a, b):
+    if b == 0:
+        raise ZeroDivisionError("No se puede dividir por cero")
+    return a / b
